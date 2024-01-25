58646250e0c87c534550a9c8e33f58e81c7e9203
Philipp <barbey@iqo.uni-hannover.de>
2024-01-25 16:34:11 +0100

    minor code cleanup

--- SpectroscopyLock/UnitConvert.cs
+++ SpectroscopyLock/UnitConvert.cs
@@ -8,6 +8,13 @@ namespace LaserLeash
     {
         const double DACLSBPerVolt = (1 << 16) / (4.096 * 5);
         const double SamplePeriod = 10e-9 * 128;
+
+        /// <summary>
+        /// Converts voltage to IIR y limit machine units
+        /// </summary>
+        /// <param name="voltage"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException"></exception>
         public static int YVToMU(double voltage)
         {
             int code = (int)Math.Round(voltage * DACLSBPerVolt, MidpointRounding.AwayFromZero);
@@ -16,25 +23,55 @@ namespace LaserLeash
             return code;
         }
 
+        /// <summary>
+        /// Converts DAC machine units to volt
+        /// </summary>
+        /// <param name="mu"></param>
+        /// <returns></returns>
         public static double DACMUToV(int mu)
         {
             return (mu^unchecked((short)0x8000))/ DACLSBPerVolt;
         }
 
+        /// <summary>
+        /// Converts ADC machine units to volt
+        /// </summary>
+        /// <param name="mu"></param>
+        /// <returns></returns>
         public static double ADCMuToV(int mu)
         {
             return   mu / DACLSBPerVolt;
         }
 
+        /// <summary>
+        /// Converts an number of samples to the sampling period in seconds
+        /// </summary>
+        /// <param name="sampleIndex"></param>
+        /// <returns></returns>
         public static double SampleToTime(int sampleIndex)
         {
             return sampleIndex * SamplePeriod;
         }
+
+        /// <summary>
+        /// Converts a sampling duration in seconds to a number of samples with integer rounding
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
         public static int TimeToSample(double time)
         {
             return (int)(time/SamplePeriod);
         }
 
+        /// <summary>
+        /// Converts PID parameters to IIR coefficients
+        /// </summary>
+        /// <param name="Kp"></param>
+        /// <param name="Ki"></param>
+        /// <param name="Kd"></param>
+        /// <param name="samplePeriod"></param>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
         public static List<double> CalculateIIR(double Kp, double Ki, double Kd, double samplePeriod)
         {
             // code based on https://github.com/quartiq/stabilizer/blob/831a9ba747b51241936b13a2df23753c03cc9dd5/py/stabilizer/iir_coefficients.py
